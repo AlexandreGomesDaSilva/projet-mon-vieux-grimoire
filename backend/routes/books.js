879e3c308561d4b2755c68a9cac119d10d4e9a3f
AlexandreGomesDaSilva <alexgds92@gmail.com>
2025-03-24 10:57:03 +0100

    Multer has been added to the middleware and has been successfully implemented on the affected routes

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const Books = require("../models/Books");
 const bookCtrl = require("../controllers/books.js");
@@ -8,9 +9,9 @@ const bookCtrl = require("../controllers/books.js");
 router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestRating);
-router.post("/", auth, bookCtrl.createBook);
+router.post("/", auth, multer, bookCtrl.createBook);
 router.post("/:id/rating", auth, bookCtrl.addRating);
-router.put("/:id", auth, bookCtrl.modifyBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
